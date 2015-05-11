461542693aeb373a5eb6f7688250596e8690465b
dylan-yuan <yuan_dylan@yahoo.com>
2015-05-10 20:53:21 -0700

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,3 +1,11 @@
+## Getting full dataset
+data_full <- read.csv("household_power_consumption.txt", header = T, sep = ';', na.strings = "?", nrows = 2075259, check.names = F, stringsAsFactors = F, comment.char = "", quote = '\"')
+data_full$Date <- as.Date(data_full$Date, format = "%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(data_full, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
 ## Generating Plot 3
 with(data, {
     plot(Sub_metering_1 ~ Datetime, type = "l", 
